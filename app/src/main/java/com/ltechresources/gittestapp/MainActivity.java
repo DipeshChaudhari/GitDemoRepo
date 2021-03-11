6b69a49ab1b103207970cfa794dca44723135365
empacc <chaudip-cont@mahindra.com>
2021-03-11 21:46:35 +0530

    New Commit

--- app/src/main/java/com/ltechresources/gittestapp/MainActivity.java
+++ app/src/main/java/com/ltechresources/gittestapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.ltechresources.gittestapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -15,5 +16,12 @@ public class MainActivity extends AppCompatActivity {
         Toast.makeText(this, "Test Demo", Toast.LENGTH_SHORT).show();
 
         System.out.println("This change is done by me");
+
+        TextView textView = findViewById(R.id.text);
+        textView.setText("This is new Data");
+
     }
+
+
+
 }
