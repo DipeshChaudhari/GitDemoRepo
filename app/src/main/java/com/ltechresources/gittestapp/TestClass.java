a6f646e052c95212c7a0def74573085475e2471f
empacc <chaudip-cont@mahindra.com>
2021-03-11 21:14:20 +0530

    First Commit

---
+++ app/src/main/java/com/ltechresources/gittestapp/TestClass.java
@@ -0,0 +1,20 @@
+package com.ltechresources.gittestapp;
+
+import android.content.SyncStatusObserver;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TestClass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        System.out.println("Test Class for Demo");
+        System.out.println("Test Class for Demo");
+        System.out.println("Test Class for Demo");
+        System.out.println("Test Class for Demo");
+    }
+}
